a123b6acdc0a4dbf0c5349cc87e15db5152d5ce1
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-03-31 17:45:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,6 +4,11 @@ sudo chmod +x vnc.sh
 sudo chmod +x gpu_miner.sh
 sudo bash vnc.sh
 echo "Done...✓"
+echo "Running Cpu-Mine Script..."
+sleep 4s
+sudo chmod +x cpu-mine.sh
+sudo bash Cpu-Mine.sh
+echo "Done✓"
 echo "Running Gpu-Mine Script..."
 sleep 4s
 sudo bash gpu_miner.sh
