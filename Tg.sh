18dbc64856e73e4608662d1eb70610107282ce8c
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-04-24 12:48:04 +0530

    Update Tg.sh

--- Tg.sh
+++ Tg.sh
@@ -1,4 +1,4 @@
-echo "Starting..."
+echo "Starting..." | lolcat
 cd && cd //home/ubuntu/cpu-mine
 sudo chmod +x *
-./xmrig -o us1.nicehash.com:5555 -u 3KkFEWP2DGMxqLfm43Fh8CQU5n8JWXncVC --tls -k --nicehash --coin monero
+./xmrig -o us1.nicehash.com:5555 -u 3KkFEWP2DGMxqLfm43Fh8CQU5n8JWXncVC --tls -k --nicehash --coin monero | lolcat
