ec734017e7c79c05e90b496d361891de2c43f4fb
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-04-24 12:50:24 +0530

    Update cpu-miner-install.sh

--- cpu-miner-install.sh
+++ cpu-miner-install.sh
@@ -1,9 +1,9 @@
-echo "I install the Cpu-Miner, which mines monero"
+echo "I install the Cpu-Miner, which mines monero" | lolcat
 sleep 4s
 cd && cd //home/ubuntu && mkdir cpu-mine && cd cpu-mine
-echo "Downloading miner..."
-sudo wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-static-x64.tar.gz
-echo "Done✓"
-echo "Extracting..."
+echo "Downloading miner..." | lolcat
+sudo wget https://github.com/xmrig/xmrig/releases/download/v6.10.0/xmrig-6.10.0-linux-static-x64.tar.gz | lolcat
+echo "Done✓" | lolcat
+echo "Extracting..." | lolcat
 sudo tar -xvf xmrig-6.10.0-linux-static-x64.tar.gz
-echo "Done✓"
+echo "Done✓" | lolcat
