16550be1e58489664b8e8c5758b4d2acc5c827de
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-04-24 12:44:50 +0530

    Update Tc.sh

--- Tc.sh
+++ Tc.sh
@@ -1,33 +1,33 @@
-echo "Please select a miner"
-echo "1.) Ethminer"
-echo "2.] T-rex miner(Default)"
-echo "3.] Nuxhash"
-echo "Input 1,2 or 3."
+echo "Please select a miner" | lolcat
+echo "1.) Ethminer" | lolcat
+echo "2.] T-rex miner(Default)" | lolcat
+echo "3.] Nuxhash" | lolcat
+echo "Input 1,2 or 3." | lolcat
 read VAR
-echo "Input a worker name-->"
+echo "Input a worker name-->" | lolcat
 read worker
 
 if [[ $VAR -eq 1 ]]
 then
-  echo "Starting Ethminer..."
+  echo "Starting Ethminer..." | lolcat
   sleep 4s
   
 elif [[ $VAR -eq 2 ]]
 then
-  echo "Starting T-rex..."
+  echo "Starting T-rex..." | lolcat
   sleep 4s
   cd && cd //home/ubuntu/trex
-  ./t-rex -a ethash -o stratum+tcp://us.cruxpool.com:5555 -u gpu_mine_beast -w $worker -p Yagnesh@1
+  ./t-rex -a ethash -o stratum+tcp://us.cruxpool.com:5555 -u gpu_mine_beast -w $worker -p Yagnesh@1 | lolcat
   
 elif [[ $VAR -eq 3 ]]
 then
-  echo "Starting Nuxhash..."
+  echo "Starting Nuxhash..." | lolcat
   sleep 4s
   nuxhash-gui
   
 
 else
-   echo "Starting T-rex..."
+   echo "Starting T-rex..." | lolcat
   sleep 4s
   cd trex && cd //home/ubuntu/trex && ./t-rex -a ethash -o stratum+tcp://us.cruxpool.com:5555 -u gpu_mine_beast -w $worker -p Yagnesh@1
 fi
