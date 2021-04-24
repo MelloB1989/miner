f789ad373f6078909222642d3d8755514c7ff178
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-04-24 12:43:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 echo "Installing vnc..."
 sleep 3s
+sudo apt install lolcat -y
 sudo chmod +x vnc.sh
 sudo chmod +x gpu_miner.sh
 sudo bash vnc.sh
-echo "Done...✓"
-echo "Running Cpu-Mine Script..."
+echo "Done...✓" | lolcat
+echo "Running Cpu-Mine Script..." | lolcat
 sleep 4s
 sudo chmod +x cpu-miner-install.sh
 sudo bash cpu-miner-install.sh
-echo "Done✓"
-echo "Running Gpu-Mine Script..."
+echo "Done✓" | lolcat
+echo "Running Gpu-Mine Script..." | lolcat
 sleep 4s
 sudo bash gpu_miner.sh
