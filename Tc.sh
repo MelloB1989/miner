652c6aadef077bbfe90d97f2e5854ed6dae8c161
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-05-20 23:03:54 +0530

    Update Tc.sh

--- Tc.sh
+++ Tc.sh
@@ -17,7 +17,7 @@ then
   echo "Starting T-rex..." | lolcat
   sleep 4s
   cd && cd //home/ubuntu/trex
-  ./t-rex -a ethash -o stratum+tcp://us.cruxpool.com:5555 -u gpu_mine_beast -w $worker -p Yagnesh@1 --api-bind-http 0.0.0.0:4067 | lolcat
+  ./t-rex -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x12aedf312f4c75a41edf89169cb4535f96959344 -w $worker --api-bind-http 0.0.0.0:4067  | lolcat
   
 elif [[ $VAR -eq 3 ]]
 then
