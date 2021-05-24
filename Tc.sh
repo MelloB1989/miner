14c863d8acde9b10e0941291d1c07949ce1a3258
Kartik Deshmukh <63499572+MelloB1989@users.noreply.github.com>
2021-05-24 18:23:46 +0530

    Update Tc.sh

--- Tc.sh
+++ Tc.sh
@@ -2,7 +2,8 @@ echo "Please select a miner" | lolcat
 echo "1.) Ethminer" | lolcat
 echo "2.] T-rex miner(Default)" | lolcat
 echo "3.] Nuxhash" | lolcat
-echo "Input 1,2 or 3." | lolcat
+echo "4.] Gminer" | lolcat
+echo "Input 1,2,3 or 4." | lolcat
 read VAR
 echo "Input a worker name-->" | lolcat
 read worker
@@ -16,30 +17,21 @@ elif [[ $VAR -eq 2 ]]
 then
   echo "Starting T-rex..." | lolcat
   sleep 4s
-  echo "Please select a pool" | lolcat
-  echo "1. Cruxpool" | lolcat
-  echo "2. Ethminer" | lolcat
-  echo "Input 1 or 2" | lolcat
-  read ool
-  if [[ $ool -eq 1 ]]
-  then
-    cd && cd //home/ubuntu/trex
-    ./t-rex -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x12aedf312f4c75a41edf89169cb4535f96959344 -w $worker --api-bind-http 0.0.0.0:4067 --api-read-only --api-bind-telnet 0 | lolcat
-  elif [[ $ool -eq 2 ]]
-    cd && cd //home/ubuntu/trex
-    ./miner --algo ethash --server eth.cruxpool.com:5555 --user 0x12aedf312f4c75a41edf89169cb4535f96959344.$worker
-    
-  else
-    echo "Not valid!"
-  fi  
+  cd && cd //home/ubuntu/trex
+  ./t-rex -a ethash -o stratum+tcp://us1.ethermine.org:4444 -u 0x12aedf312f4c75a41edf89169cb4535f96959344 -w $worker --api-bind-http 0.0.0.0:4067 --api-read-only --api-bind-telnet 0 | lolcat
   
 elif [[ $VAR -eq 3 ]]
 then
   echo "Starting Nuxhash..." | lolcat
   sleep 4s
   nuxhash-gui
-  
 
+elif [[ $VAR -eq 4 ]]
+then
+  echo "Starting Gminer..." | lolcat
+  cd && cd //home/ubuntu/gminer
+  ./miner --algo ethash --server us1.ethermine.org:4444 --user 0x12aedf312f4c75a41edf89169cb4535f96959344.$worker
+  
 else
    echo "Starting T-rex..." | lolcat
   sleep 4s
